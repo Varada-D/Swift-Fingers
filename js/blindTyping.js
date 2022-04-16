ef05960427673a8c48fbe0205707ed7f3f5c6c42
Varada Deshpande <81087717+Varada-D@users.noreply.github.com>
2022-04-16 18:01:58 +0530

    add scores, its animations to all rounds

--- js/blindTyping.js
+++ js/blindTyping.js
@@ -13,9 +13,12 @@ let timer,
     timeLeft = maxTime,
     wrongWords = isTyping = 0;
 
+let scoreShown = false
+
 function loadParagraph() {
     const ranIndex = Math.floor(Math.random() * paragraphs.length);
     typingText.innerHTML = "";
+    // "have all without govern or turn plan tell interest such".split(" ").forEach(word => {
     paragraphs[ranIndex].split(" ").forEach(word => {
         let div = ''
         word.split("").forEach(char => {
@@ -25,14 +28,14 @@ function loadParagraph() {
         typingText.innerHTML += `<span class="char"> </span>`
     });
     typingText.getElementsByClassName("word")[0].classList.add("active");
-    document.addEventListener("keydown", () => inpField.focus());
-    typingText.addEventListener("click", () => inpField.focus());
+    document.addEventListener("keydown", () => { inpField.focus(); initTyping() });
+    typingText.addEventListener("click", () => { inpField.focus(); initTyping() });
 }
 
 let word = '', wordElement = ''
 
 function initTyping() {
-    if (wordIndex < typingText.getElementsByClassName("word").length - 1 && timeLeft > 0) {
+    if (wordIndex <= typingText.getElementsByClassName("word").length - 1 && timeLeft > 0) {
         if (!isTyping) {
             timer = setInterval(initTimer, 1000);
             isTyping = true;
@@ -43,68 +46,150 @@ function initTyping() {
         for (i = 0; i < characters.length; i++) {
             word += characters[i].innerText;
         }
-        if (wordIndex >= typingText.getElementsByClassName("word").length - 1) {
+        if (wordIndex > typingText.getElementsByClassName("word").length - 1) {
             clearInterval(timer);
         }
+        // console.log()
     } else {
         clearInterval(timer);
         inpField.disabled = true
-        document.querySelector("li.mistake").style.display = "inline-block"
-        document.querySelector("li.accuracy").style.display = "inline-block"
-        document.querySelector("li.wpm").style.display = "inline-block"
+        // mistakeTag.innerText = wrongWords;
+        // wpmTag.innerText = Math.round(((totalWords - wrongWords) / 5) / (maxTime - timeLeft) * 60)
+        // accuracyTag.innerText = ((1 - (wrongWords / totalWords)) * 100).toFixed(0) + "%"
+        // document.querySelector("li.mistake").style.display = "inline-block"
+        // document.querySelector("li.accuracy").style.display = "inline-block"
+        // document.querySelector("li.wpm").style.display = "inline-block"
+
+        inpField.disabled = true;
+        if (!scoreShown) {
+            scrollTo(0, 0)
+            document.querySelector('body').style.overflowY = 'hidden'
+            document.querySelector('main').style.visibility = 'hidden'
+            document.querySelector('main').style.opacity = '0'
+            document.querySelector('main').style.top = '100vh'
+            // document.querySelector('main').style.paddingTop = '100vh'
+            // console.log(totalWords)
+            // console.log(wrongWords)
+            // console.log(maxTime - timeLeft)
+            // console.log(((totalWords - wrongWords) / 5) / (maxTime - timeLeft) * 60)
+            // console.log(((totalWords * (1 - (wrongWords / totalWords)) * 100) / 5) / (maxTime - timeLeft) * 60)
+            setTimeout(function () {
+                document.querySelector('body').style.overflowY = 'auto'
+                document.querySelector('main').innerHTML = `<section class="container mt-0 py-5">
+                    <div class="row mt-0 py-5">
+                        <div class="col-12 my-auto mt-0 py-5">
+                            <div class="row text-center mt-0 py-3">
+                                <h1 class="col-12 text-center">Your Scores for the Round</h1>
+                                <div class="wrapper">
+                                    <div class="content-box">
+                                        <div class="content"
+                                            style="min-height: 44.8vh; width: 80%; margin-left: auto; margin-right: auto;">
+        
+                                            <div class="accuracy d-flex align-items-center flex-column">
+                                                <div class="my-auto">
+                                                    <i class="bi bi-md bi-bullseye result_icon"></i>
+                                                </div>
+                                                <label style="text-align: center; display: block; height: 30px;">${((1 - (wrongWords / totalWords)) * 100).toFixed(0)}%</label>
+                                                <label style="text-align: center; display: block;">Accuracy</label>
+                                            </div>
+        
+                                            <div class="wpm d-flex align-items-center flex-column"
+                                                style=" transform: scale(120%);">
+                                                <div style="margin-top:-15px; margin-bottom: 2vh;">
+                                                    <i class="bi bi-speedometer2 result_icon"></i>
+                                                </div>
+                                                <label style="text-align: center; display: block; height: 20px;">${Math.round(((totalWords - wrongWords) / 5) / (maxTime - timeLeft) * 60)}</label>
+                                                <label style="text-align: center; display: block;">WPM</label>
+                                            </div>
+        
+                                            <div class="time d-flex align-items-center flex-column">
+                                                <div style="margin-top:-15px; margin-bottom: 2vh;">
+                                                    <i class="bi bi-clock-history result_icon"></i>
+                                                </div>
+                                                <label style="text-align: center; display: block; height: 20px;">${(maxTime - timeLeft).toFixed(1)}</label>
+                                                <label style="text-align: center; display: block;">Seconds</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 text-center nxt">
+                                <a class="btn btn-primary" href="score.html">View Result</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>`
+                document.querySelector('.title').classList.add('col-12')
+                document.querySelector('.title').style.paddingLeft = '40vw'
+                document.querySelector('main').style.visibility = 'visible'
+                document.querySelector('main').style.opacity = '1'
+                document.querySelector('main').style.top = '0'
+                document.querySelector('main').style.paddingTop = '2.5rem'
+                document.querySelector('.content').style.borderBottom = 'none'
+                document.querySelector('footer').style.visibility = 'visible'
+                document.querySelector('footer').style.opacity = '1'
+                document.querySelector(".time").style.visibility = "visible"
+                document.querySelector(".time").style.opacity = "1"
+                document.querySelector(".accuracy").style.visibility = "visible"
+                document.querySelector(".accuracy").style.opacity = "1"
+                document.querySelector(".wpm").style.visibility = "visible"
+                document.querySelector(".wpm").style.opacity = "1"
+                setTimeout(function () {
+                    document.querySelector('.nxt').style.visibility = 'visible'
+                    document.querySelector('.nxt').style.opacity = '1'
+                }, 500)
+            }, 1500)
+            scoreShown = true;
+        }
     }
 }
 
 function checkCalc() {
     let inpFieldVal = inpField.value;
-    // if (inpFieldVal.length<word.length)
-    //     inpFieldVal.padEnd(word.length, "*")
-    // else if (inpFieldVal.length>word.length)
-    //     word.padEnd(inpFieldVal.length, "*")
+    inpField.value = ''
+    word = word + " "
+    inpFieldVal = inpFieldVal + " "
+    if (wordIndex != 0) {
+        word = " " + word
+    }
+    // console.log(inpFieldVal)
+    // console.log(word)
     let correct = true
-    if (inpFieldVal.length-1 != word.length) {
+    if (inpFieldVal.length != word.length) {
         correct = false
     }
-    if (inpFieldVal.length-1 > word.length) {
+    if (inpFieldVal.length - 1 > word.length) {
         // correct = false
-        totalWords += (inpFieldVal.length-1 - word.length)
-        wrongWords += (inpFieldVal.length-1 - word.length)
+        totalWords += (inpFieldVal.length - 1 - word.length)
+        wrongWords += (inpFieldVal.length - 1 - word.length)
     }
     for (i = 0; i < word.length; i++) {
         if (inpFieldVal[i] != word[i]) {
-            // correct = false
+            correct = false
             wrongWords++
         }
         totalWords++
     }
-    // totalWords--
-    console.log(totalWords)
     if (correct) {
         wordElement.classList.add("correct")
     }
     else {
         wordElement.classList.add("incorrect")
     }
-    inpField.dispatchEvent(new KeyboardEvent('keydown', { 'key': ' ' }))
-    inpField.value = ''
-    let wpm = (((totalWords - wrongWords) / 5) / (maxTime - timeLeft) * 60).toFixed(0);
-    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
-    wpmTag.innerText = wpm;
-    mistakeTag.innerText = wrongWords;
-    accuracyTag.innerText = ((1 - (wrongWords / totalWords)) * 100).toFixed(0) + "%"
     wordIndex++
     for (i = 0; i < typingText.getElementsByClassName("word").length; i++) {
         typingText.getElementsByClassName("word")[i].classList.remove("active")
     }
-    typingText.getElementsByClassName("word")[wordIndex].classList.add("active")
+    if (typingText.getElementsByClassName("word")[wordIndex] != null) {
+        typingText.getElementsByClassName("word")[wordIndex].classList.add("active")
+    }
 }
 
 function initTimer() {
     if (timeLeft > 0) {
         timeLeft--;
+        // console.log(timeLeft)
         timeTag.innerText = timeLeft;
-        let wpm = (((totalWords - wrongWords) / 5) / (maxTime - timeLeft) * 60).toFixed(0);
-        wpmTag.innerText = wpm;
     } else {
         clearInterval(timer);
         inpField.disabled = true
@@ -116,11 +201,13 @@ function initTimer() {
 
 
 loadParagraph();
-inpField.addEventListener("keyup", function (e) {
+inpField.addEventListener("keydown", function (e) {
     e = e.key
     initTyping()
     if (e == ' ') {
         // totalWords++
-        checkCalc()
+        if (typingText.getElementsByClassName("word")[wordIndex] != null) {
+            checkCalc()
+        }
     }
 });
--- js/speedRun.js
+++ js/speedRun.js
@@ -7,13 +7,14 @@ const typingText = document.querySelector(".typing-text p"),
 
 let totalWords = 0
 
-let timer = 0,
+let timer = 1,
     charIndex = wrongWords = isTyping = 0, charLen = 0;
 
 function loadParagraph() {
     const ranIndex = Math.floor(Math.random() * paragraphs.length);
     typingText.innerHTML = "";
-        paragraphs[ranIndex].split(" ").forEach(word => {
+    // "have all without govern or turn plan tell interest such".split(" ").forEach(word => {
+    paragraphs[ranIndex].split(" ").forEach(word => {
         let div = ''
         word.split("").forEach(char => {
             div += `<span class="char">${char}</span>`;
@@ -32,7 +33,7 @@ function initTyping() {
     let typedChar = inpField.value.split("")[charIndex];
     if (charIndex < characters.length - 1) {
         if (!isTyping) {
-            timer = setInterval(initTimer, 100);
+            timer = setInterval(initTimer, 1000);
             isTyping = true;
         }
         if (typedChar == null) {
@@ -54,38 +55,104 @@ function initTyping() {
         }
         characters.forEach(span => span.classList.remove("active"));
         characters[charIndex].classList.add("active");
-        let wpm = Math.round(((charIndex - wrongWords) / 5) / (timer) * 60);
-        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
-        wpmTag.innerText = wpm;
-        mistakeTag.innerText = wrongWords;
-        accuracyTag.innerText = ((1 - (wrongWords / totalWords)) * 100).toFixed(0) + "%"
-        console.log(characters.length)
-
+        // let wpm = Math.round(((charIndex - wrongWords) / 5) / (timer) * 60);
+        // wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        // wpmTag.innerText = wpm;
+        // mistakeTag.innerText = wrongWords;
+        // accuracyTag.innerText = ((1 - (wrongWords / totalWords)) * 100).toFixed(0) + "%"
     }
     else {
         isTyping = false;
         clearInterval(timer);
         inpField.value = "";
-        document.querySelector("li.mistake").style.display = "inline-block"
-        document.querySelector("li.accuracy").style.display = "inline-block"
-        document.querySelector("li.wpm").style.display = "inline-block"
+        inpField.disabled = true;
+        scrollTo(0, 0)
+        document.querySelector('body').style.overflowY = 'hidden'
+        document.querySelector('main').style.visibility = 'hidden'
+        document.querySelector('main').style.opacity = '0'
+        document.querySelector('main').style.top = '100vh'
+        document.querySelector('main').style.paddingTop = '100vh'
+        setTimeout(function () {
+            document.querySelector('body').style.overflowY = 'auto'
+            document.querySelector('main').innerHTML = `<section class="container mt-0 py-5">
+                        <div class="row mt-0 py-5">
+                            <div class="col-12 my-auto mt-0 py-5">
+                                <div class="row text-center mt-0 py-3">
+                                    <h1 class="col-12 text-center">Your Scores for the Round</h1>
+                                    <div class="wrapper">
+                                        <div class="content-box">
+                                            <div class="content"
+                                                style="min-height: 44.8vh; width: 80%; margin-left: auto; margin-right: auto;">
+            
+                                                <div class="accuracy d-flex align-items-center flex-column">
+                                                    <div class="my-auto">
+                                                        <i class="bi bi-md bi-bullseye result_icon"></i>
+                                                    </div>
+                                                    <label style="text-align: center; display: block; height: 30px;">${((1 - (wrongWords / totalWords)) * 100).toFixed(0)}%</label>
+                                                    <label style="text-align: center; display: block;">Accuracy</label>
+                                                </div>
+            
+                                                <div class="wpm d-flex align-items-center flex-column"
+                                                    style=" transform: scale(120%);">
+                                                    <div style="margin-top:-15px; margin-bottom: 2vh;">
+                                                        <i class="bi bi-speedometer2 result_icon"></i>
+                                                    </div>
+                                                    <label style="text-align: center; display: block; height: 20px;">${Math.round(((charIndex - wrongWords) / 5) / (timer - 1) * 60)}</label>
+                                                    <label style="text-align: center; display: block;">WPM</label>
+                                                </div>
+            
+                                                <div class="time d-flex align-items-center flex-column">
+                                                    <div style="margin-top:-15px; margin-bottom: 2vh;">
+                                                        <i class="bi bi-clock-history result_icon"></i>
+                                                    </div>
+                                                    <label style="text-align: center; display: block; height: 20px;">${((timer - 1) / 60).toFixed(0) + ":" + ((timer - 1) % 60)}</label>
+                                                    <label style="text-align: center; display: block;">Minutes</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 text-center nxt">
+                                    <a class="btn btn-primary" href="timescape.html">Next Round</a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>`
+            document.querySelector('.title').classList.add('col-12')
+            document.querySelector('.title').style.paddingLeft = '40vw'
+            document.querySelector('main').style.visibility = 'visible'
+            document.querySelector('main').style.opacity = '1'
+            document.querySelector('main').style.top = '0'
+            document.querySelector('main').style.paddingTop = '2.5rem'
+            document.querySelector('.content').style.borderBottom = 'none'
+            document.querySelector('footer').style.visibility = 'visible'
+            document.querySelector('footer').style.opacity = '1'
+            document.querySelector(".time").style.visibility = "visible"
+            document.querySelector(".time").style.opacity = "1"
+            document.querySelector(".accuracy").style.visibility = "visible"
+            document.querySelector(".accuracy").style.opacity = "1"
+            document.querySelector(".wpm").style.visibility = "visible"
+            document.querySelector(".wpm").style.opacity = "1"
+            setTimeout(function () {
+                document.querySelector('.nxt').style.visibility = 'visible'
+                document.querySelector('.nxt').style.opacity = '1'
+            }, 500)
+        }, 1500)
     }
 }
 
 function initTimer() {
-    timeTag.innerText = timer.toFixed(1);
+    timeTag.innerText = timer - 1;
     if (charIndex >= charLen - 1) {
         clearInterval(timer);
-        document.querySelector("li.mistake").style.display = "inline-block"
-        document.querySelector("li.accuracy").style.display = "inline-block"
-        document.querySelector("li.wpm").style.display = "inline-block"
     }
     else {
-        timer+=0.1;
-        let wpm = Math.round(((charIndex - wrongWords) / 5) / (timer) * 60);
+        timer++;
+        let wpm = Math.round(((charIndex - wrongWords) / 5) / (timer - 1) * 60);
         wpmTag.innerText = wpm;
     }
 }
 
 loadParagraph();
+inpField.disabled = false
 inpField.addEventListener("input", initTyping);
